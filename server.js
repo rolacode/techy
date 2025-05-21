48174035ceb16b3c266354cdfd8929a5c24f8933
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-05-21 11:33:14 -0700

    server.js was reconfigured

--- server.js
+++ server.js
@@ -68,4 +68,6 @@ mongoose
     .catch((err) => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
