3ffe9de8172d8a10c26e99d870d1e8ac2acd8ab6
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-05-21 13:31:48 -0700

    update on server.js

--- server.js
+++ server.js
@@ -67,7 +67,6 @@ mongoose
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
 });
