118aecb922036a90256220b55d8b71e0d613dc30
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-05-26 18:15:01 -0700

    update on index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // 🚀 Start HTTP server (important: use server.listen)
 const PORT = process.env.PORT || 5000;
-const HOST = 'localhost';
+const HOST = '0.0.0.0';
 
 server.listen(PORT, HOST, () => {
   console.log(`✅ Server is running at http://${HOST}:${PORT}`);
