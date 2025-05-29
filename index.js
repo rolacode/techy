954bf72d9b5dd838e94bcf689a144c03edb6f259
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-05-29 10:30:10 -0700

    update cors for better updating

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
