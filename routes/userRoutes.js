4ccb40a3d783ba838c5794a87ca4afb7d5fdb7ab
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-05-29 01:40:19 -0700

    update user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const { registerUser, login, getDoctor, updatePatientVitals, getPatientProfile, } = require('../controllers/userController');
 const multer = require('../middleware/multer');
-const protect = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
+
+console.log('getPatientProfile:', getPatientProfile);
+console.log('protect:', protect);
 
 router.post('/register', multer.single('image'), registerUser);
 router.post('/login', login);
 router.get('/doctors', getDoctor);
 router.get('/patient/profile', protect, getPatientProfile);
 router.patch('/patients/:id/vitals', protect, updatePatientVitals);
-
 
 module.exports = router;
